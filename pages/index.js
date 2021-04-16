031f4a4e71f1ffadf3bfe68a744357dbb9d38801
lucasadlerstein <lucasadlerstein@gmail.com>
2021-04-16 08:35:30 -0300

    NewLogo 16

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
 
-        <link
+          <link
             rel="preload"
             href="/fonts/GlacialIndifference-Bold.otf"
             as="font"
